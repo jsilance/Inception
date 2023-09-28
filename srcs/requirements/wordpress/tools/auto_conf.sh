ff954734e6cc444fe252b7b0274c25c7a578e511
Julien Silance <jusilanc@fu-c3-p5.s19.be>
2023-09-28 12:35:21 +0200

    maj final

--- srcs/requirements/wordpress/tools/auto_conf.sh
+++ srcs/requirements/wordpress/tools/auto_conf.sh
@@ -5,7 +5,8 @@ if [ $? -eq 0 ]; then
 else
     echo "Create config"
     wp config create --dbname=$SQL_DATABASE_NAME --dbuser=$SQL_USER --dbpass=$SQL_PASSWORD --dbhost=$SQL_DATABASE_HOST
-    wp core install --url=$DOMAIN_NAME --title="$WP_TITTLE" --admin_user=$WP_USER --admin_password=$WP_PASSWORD --admin_email=$WP_EMAIL
+    wp core install --url=$DOMAIN_NAME --title="$WP_TITTLE" --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_EMAIL
+    wp user create $WP_USER_NAME $WP_USER_EMAIL --user_pass=$WP_USER_PASSWORD --display_name=$WP_USER_DISPLAY_NAME 
 fi
 
 php-fpm7.4 -F
